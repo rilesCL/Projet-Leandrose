119161de92c94e4f955da8446852e292e576e044
lanfiadrso <lanfiadiarisso123@gmail.com>
2025-09-16 16:41:37 -0400

    Ajout de validation des champs d'inscription Employeur

--- src/main/java/ca/cal/leandrose/presentation/RegisterController.java
+++ src/main/java/ca/cal/leandrose/presentation/RegisterController.java
@@ -3,6 +3,7 @@ package ca.cal.leandrose.presentation;
 import ca.cal.leandrose.presentation.request.RegisterEmployeur;
 import ca.cal.leandrose.service.EmployeurService;
 import ca.cal.leandrose.service.dto.EmployeurDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,7 @@ public class RegisterController {
     private final EmployeurService employeurService;
 
     @PostMapping("/employeur")
-    public ResponseEntity<EmployeurDto> registerEmployeur(@RequestBody RegisterEmployeur request) {
+    public ResponseEntity<EmployeurDto> registerEmployeur(@Valid @RequestBody RegisterEmployeur request) {
         EmployeurDto employeurDto = employeurService.createEmployeur(
                 request.getFirstName(),
                 request.getLastName(),
@@ -27,6 +28,7 @@ public class RegisterController {
         );
         return ResponseEntity.status(HttpStatus.CREATED).body(employeurDto);
     }
+
 
 
 }
--- src/main/java/ca/cal/leandrose/presentation/request/RegisterEmployeur.java
+++ src/main/java/ca/cal/leandrose/presentation/request/RegisterEmployeur.java
@@ -1,15 +1,33 @@
 package ca.cal.leandrose.presentation.request;
 
-import lombok.Data;
-import java.time.LocalDate;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class RegisterEmployeur {
+
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
+    @Email(message = "Email must be valid")
+    @NotBlank
     private String email;
+
+    @NotBlank
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
+
+    @NotBlank
     private String companyName;
+
+    @NotBlank
     private String field;
-    private LocalDate since;
 }
+
