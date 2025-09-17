ddbe8a22f0c89c8c25bc9ba0851dc1d8814757fb
Ghilas Amarouche <6194975@claurendeau.qc.ca>
2025-09-17 12:51:13 -0400

    student dto avec héritage de userDto

--- src/main/java/ca/cal/leandrose/service/dto/StudentDto.java
+++ src/main/java/ca/cal/leandrose/service/dto/StudentDto.java
@@ -1,5 +1,38 @@
 package ca.cal.leandrose.service.dto;
 
-public class StudentDto {
+import ca.cal.leandrose.model.Student;
+import ca.cal.leandrose.model.auth.Role;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class StudentDto extends UserDTO{
+    private String studentNumber;
+    private String program;
+
+    @Builder
+    public StudentDto(Long id, String firstName, String lastName, String email, Role role, String studentNumber, String program) {
+        super(id, firstName, lastName, email, role);
+        this.studentNumber = studentNumber;
+        this.program = program;
+    }
+
+    public StudentDto(){}
+
+    public static StudentDto create(Student student){
+        return StudentDto.builder()
+                .id(student.getId())
+                .firstName(student.getFirstName())
+                .lastName(student.getLastName())
+                .email(student.getEmail())
+                .role(student.getRole())
+                .studentNumber(student.getStudentNumber())
+                .program(student.getProgram())
+                .build();
+    }
+
+    public static StudentDto empty() {
+        return new StudentDto();
+    }
 
 }
