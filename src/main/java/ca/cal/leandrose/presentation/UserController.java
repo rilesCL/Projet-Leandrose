20106ec1b2028948b4180df1c6fdde4550c73f1e
lanfiadrso <lanfiadiarisso123@gmail.com>
2025-09-22 00:33:06 -0400

    Implémentation des methodes controleur d'offre de stage

--- src/main/java/ca/cal/leandrose/presentation/UserController.java
+++ src/main/java/ca/cal/leandrose/presentation/UserController.java
@@ -1,6 +1,10 @@
 package ca.cal.leandrose.presentation;
 
+import ca.cal.leandrose.model.Employeur;
+import ca.cal.leandrose.model.InternshipOffer;
+import ca.cal.leandrose.presentation.request.InternshipOfferRequest;
 import ca.cal.leandrose.service.AuthService;
+import ca.cal.leandrose.service.InternshipOfferService;
 import ca.cal.leandrose.service.UserAppService;
 import ca.cal.leandrose.service.dto.JWTAuthResponse;
 import ca.cal.leandrose.service.dto.LoginDTO;
@@ -11,6 +15,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,6 +28,7 @@ public class UserController {
 
     private final AuthService authService;
     private final UserAppService userService;
+    private final InternshipOfferService internshipOfferService;
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@RequestBody LoginDTO loginDto) {
@@ -42,4 +52,48 @@ public class UserController {
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(userService.getMe(authHeader));
     }
+    @GetMapping("/employeur/offers")
+    public ResponseEntity<List<InternshipOffer>> getMyOffers(HttpServletRequest request) {
+        UserDTO me = userService.getMe(request.getHeader("Authorization"));
+
+        if (!me.getRole().name().equals("ROLE_EMPLOYEUR")) {
+            return ResponseEntity.status(403).build();
+        }
+
+        List<InternshipOffer> offers = internshipOfferService.getOffersByEmployeurId(me.getId());
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(offers);
+    }
+
+    @PostMapping(value = "/employeur/offers", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<InternshipOffer> uploadOffer(
+            HttpServletRequest request,
+            @RequestPart("offer") InternshipOfferRequest offerRequest,
+            @RequestPart("pdfFile") MultipartFile pdfFile
+    ) throws IOException {
+        UserDTO me = userService.getMe(request.getHeader("Authorization"));
+
+        if (!me.getRole().name().equals("EMPLOYEUR")) {
+            return ResponseEntity.status(403).build();
+        }
+
+        Employeur employeur = new Employeur();
+        employeur.setId(me.getId());
+
+        InternshipOffer offer = internshipOfferService.createOffer(
+                offerRequest.getDescription(),
+                LocalDate.parse(offerRequest.getStartDate()),
+                offerRequest.getDurationInWeeks(),
+                offerRequest.getAddress(),
+                offerRequest.getRemuneration(),
+                employeur,
+                pdfFile
+        );
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(offer);
+    }
+
 }
--- src/main/java/ca/cal/leandrose/presentation/request/InternshipOfferRequest.java
+++ src/main/java/ca/cal/leandrose/presentation/request/InternshipOfferRequest.java
@@ -1,4 +1,12 @@
 package ca.cal.leandrose.presentation.request;
 
+import lombok.Data;
+
+@Data
 public class InternshipOfferRequest {
+    private String description;
+    private String startDate;       // format: YYYY-MM-DD
+    private int durationInWeeks;
+    private String address;
+    private Float remuneration;
 }
