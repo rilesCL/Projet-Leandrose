9aef17afe63eb89fcb955fc1fb0b9c6debba9732
hakerboiiii <tahmeedshahed@gmail.com>
2025-09-24 15:11:53 -0400

    Création d'un controlleur ManagerController.

---
+++ src/main/java/ca/cal/leandrose/presentation/ManagerController.java
@@ -0,0 +1,35 @@
+
+package ca.cal.leandrose.presentation;
+
+
+import ca.cal.leandrose.service.ManagerService;
+import ca.cal.leandrose.service.dto.CvDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/manager")
+@RequiredArgsConstructor
+public class ManagerController {
+    private final ManagerService service;
+
+    @PostMapping("/cv/{cvId}/approve")
+    public ResponseEntity<CvDto> approveCv(@PathVariable Long cvId){
+        return ResponseEntity.ok(service.approveCv(cvId));
+    }
+
+    @PostMapping("/cv/{cvId}/reject")
+    public ResponseEntity<CvDto> rejectCv(@PathVariable Long cvId){
+        return ResponseEntity.ok(service.rejectCv(cvId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CvDto>> getPendingCvs(){
+        return ResponseEntity.ok(service.getPendingCvs());
+    }
+}
+
+
