da7ce5a57abe493082f8245142a807b3d6d6b4e7
Ghilas Amarouche <6194975@claurendeau.qc.ca>
2025-09-24 19:20:53 -0400

    use case 1 & 2

--- src/main/java/ca/cal/leandrose/presentation/GestionnaireController.java
+++ src/main/java/ca/cal/leandrose/presentation/GestionnaireController.java
@@ -1,12 +1,31 @@
 package ca.cal.leandrose.presentation;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ca.cal.leandrose.model.InternshipOffer;
+import ca.cal.leandrose.service.InternshipOfferService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/gestionnaire")
 @CrossOrigin(origins = "http://localhost:5173")
+@RequiredArgsConstructor
 public class GestionnaireController {
+    private final InternshipOfferService internshipOfferService;
 
+    // Use Case 1: Liste des offres en attente
+    @GetMapping("/offers/pending")
+    public ResponseEntity<List<InternshipOffer>> getPendingOffers() {
+        List<InternshipOffer> offers = internshipOfferService.getPendingOffersWithDetails();
+        return ResponseEntity.ok(offers);
+    }
+
+    // Use Case 2: Consulter le détail d'une offre
+    @GetMapping("/offers/{id}")
+    public ResponseEntity<InternshipOffer> getOfferDetails(@PathVariable Long id) {
+        InternshipOffer offer = internshipOfferService.getOfferDetails(id);
+        return ResponseEntity.ok(offer);
+    }
 }
