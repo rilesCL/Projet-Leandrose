f16e0cb1419bb3a609ec3af35f563638ad0f145a
lanfiadrso <lanfiadiarisso123@gmail.com>
2025-09-30 15:24:13 -0400

    Impl du dto de program

--- src/main/java/ca/cal/leandrose/service/dto/ProgramDto.java
+++ src/main/java/ca/cal/leandrose/service/dto/ProgramDto.java
@@ -1,4 +1,18 @@
 package ca.cal.leandrose.service.dto;
 
+import ca.cal.leandrose.model.Program;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ProgramDto {
+    private Long id;
+    private String name;
+
+    public static ProgramDto create(Program program){
+        return new ProgramDto(program.getId(), program.getName());
+    }
 }
