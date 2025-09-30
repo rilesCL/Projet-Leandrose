d9efc40d6c9e999da6d7b50b69bd031dd11dbed3
lanfiadrso <lanfiadiarisso123@gmail.com>
2025-09-30 15:22:48 -0400

    Impl du repo de program

---
+++ src/main/java/ca/cal/leandrose/repository/ProgramRepository.java
@@ -0,0 +1,6 @@
+package ca.cal.leandrose.repository;
+
+import ca.cal.leandrose.model.Program;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProgramRepository extends JpaRepository<Program, Long> {}
