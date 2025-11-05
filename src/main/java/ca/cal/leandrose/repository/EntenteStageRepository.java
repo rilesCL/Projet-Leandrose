ca90f195ce1f709874b977832bc2797fbba3fdef
Iliesse Aisset <2166077@claurendeau.qc.ca>
2025-11-05 14:43:48 -0500

    On ajoute les methodes services et repository

--- src/main/java/ca/cal/leandrose/repository/EntenteStageRepository.java
+++ src/main/java/ca/cal/leandrose/repository/EntenteStageRepository.java
@@ -4,8 +4,11 @@ import ca.cal.leandrose.model.EntenteStage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EntenteStageRepository extends JpaRepository<EntenteStage, Long> {
+    List<EntenteStage> findAllByProf_Id(Long profId);
 
   boolean existsByCandidatureId(Long candidatureId);
 }
