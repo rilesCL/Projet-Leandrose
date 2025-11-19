da6dae9908a7e976c40a336c7aba6bf2ed8ab23f
Iliesse Aisset <2166077@claurendeau.qc.ca>
2025-11-19 18:26:22 -0500

    Ajout de @Data pour des get set

--- src/main/java/ca/cal/leandrose/model/Gestionnaire.java
+++ src/main/java/ca/cal/leandrose/model/Gestionnaire.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @DiscriminatorValue("G")
 @Getter
 @NoArgsConstructor
+@Data
 public class Gestionnaire extends UserApp {
   @Column(unique = true, nullable = false)
   private String phoneNumber;
